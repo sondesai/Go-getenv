73a443f8a76803a898e7a2159796854d66709d12
sondesai <37583203+sondesai@users.noreply.github.com>
2022-03-01 13:22:30 +0530

    Update prnenv.go
    Added appropriate comments explaining functionality as per review comments.

--- prnenv.go
+++ prnenv.go
@@ -1,12 +1,17 @@
 package main
-
+// Import required packages
 import (
     "fmt"
     "os"
 )
 
+// Main function - entry point
 func main() {
 
+    /*
+     * Get list of all environmental variables. Go through each
+     * using a for loop and print it.
+     */
     for _, e := range os.Environ() {
         fmt.Printf("%s\n",e)
     }
